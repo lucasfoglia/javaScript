16e0fa2b4b10d9167b5119df8c11d8042ea424da
lucas foglia <lucas_foglia@hotmail.com>
2022-05-25 16:30:52 -0300

    se agrega primer desafio

--- styles.js
+++ styles.js
@@ -1,5 +1,21 @@
-let i = 0;
-while (i < 10) {
-alert (i)
-i++;
+let sumaEdades = 0;
+let cantidadDePersonas = 0;
+let respuesta = "si";
+
+function calcularPromedio() {
+  return (sumaEdades / cantidadDePersonas).toFixed(2);
+}
+
+while (respuesta === "si") {
+  sumaEdades += Number(prompt("Ingrese edad de la persona:  "));
+  cantidadDePersonas++;
+  respuesta = prompt("Quiere ingresar otra edad? conteste si o no ");
+}
+
+if (cantidadDePersonas > 0) {
+  let promedio = calcularPromedio();
+  alert("El promedio de edades registrado es de " + promedio);
+} else {
+  alert("No se puede calcular el promedio");
+
 }
