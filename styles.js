a10d56f01ba67d44447cdfcd61c04f2070604955
lucas foglia <lucas_foglia@hotmail.com>
2022-06-05 19:26:24 -0300

    se incorpora segundo desafio complementario

--- styles.js
+++ styles.js
@@ -1,19 +1,26 @@
 let sumaEdades = 0;
 let cantidadDePersonas = 0;
 let respuesta = "si";
+ const arrayEdades = [];
+
+
+
 
 function calcularPromedio() {
   return (sumaEdades / cantidadDePersonas).toFixed(2);
 }
 
 while (respuesta === "si") {
-  sumaEdades += Number(prompt("Ingrese edad de la persona:  "));
+  const entrada = Number(prompt("Ingrese edad de la persona:  "));
+  sumaEdades += entrada;
+  arrayEdades.push(entrada);
   cantidadDePersonas++;
   respuesta = prompt("Quiere ingresar otra edad? conteste si o no ");
 }
 
 if (cantidadDePersonas > 0) {
   let promedio = calcularPromedio();
+  alert("Se ingresaron " + arrayEdades.length + " Edades");
   alert("El promedio de edades registrado es de " + promedio);
 } else {
   alert("No se puede calcular el promedio");
