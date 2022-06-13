efac65d62f8c14644f8b9a8a052e1466121ee035
lucas foglia <lucas_foglia@hotmail.com>
2022-06-13 19:48:10 -0300

    se agrega desafio 3

--- styles.js
+++ styles.js
@@ -2,15 +2,31 @@ let respuesta = "si";
 const arrayPersonas = [];
 
 function calcularPromedio() {
-  return arrayPersonas.reduce((acumulador, actual) => acumulador + actual.edad, 0) / arrayPersonas.length;
+  return (
+    arrayPersonas.reduce((acumulador, actual) => acumulador + actual.edad, 0) /
+    arrayPersonas.length
+  );
 }
 
 while (respuesta === "si") {
   const nombre = prompt("Ingrese nombre de la persona");
   const edad = Number(prompt("Ingrese edad de la persona:  "));
-  arrayPersonas.push({ edad, nombre });
+
+  const persona = { edad, nombre };
+  const elementoPersona = document.createElement("li");
+
+  elementoPersona.innerHTML =
+    "nombre: " + persona.nombre + " edad: " + persona.edad;
+
+  const listaPersonas = document.getElementById("contenedorDePersonas");
+  listaPersonas.appendChild(elementoPersona);
+
+  arrayPersonas.push(persona);
   respuesta = prompt("Quiere ingresar otra edad? conteste si o no ");
 }
+const elemento = document.getElementById("cantidadDePersonas");
+
+elemento.innerHTML = "Cantidad de personas ingresadas: " + arrayPersonas.length;
 
 if (arrayPersonas.length) {
   const promedio = calcularPromedio();
